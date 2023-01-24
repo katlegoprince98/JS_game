9e1b04d83fc08cc10609240572e79bff565cda6b
princekatlego98 <you@example.com>
2023-01-24 04:54:41 -0800

    js

---
+++ game.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () =>{
+   const tiles = Array.from(document.querySelectorAll('.tile'));
+   const playerDisplay = Array.from(document.querySelectorAll('.player-display'));
+   const resetButton = Array.from(document.querySelectorAll('.#reset'));
+   const announcer = Array.from(document.querySelectorAll('.announcer'));
+});
